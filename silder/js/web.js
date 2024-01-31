4bfc534d7e85cabc2c528cd07ded7981b53bc9fc
Shahriar Akib <129962386+ShahriarAkib37@users.noreply.github.com>
2024-01-31 11:04:44 +0600

    Add files via upload

---
+++ silder/js/web.js
@@ -0,0 +1,22 @@
+$(function(){
+
+// silder //
+
+$('.silderBox').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    infinite: true,
+     speed: 500,
+     fade: true,
+     prevArrow:(`.left`),
+     nextArrow:(`.right`),
+  });
+
+// silder // 
+
+})
+
+// // audio//
+// window.addEventListener(`click`,() => {
+//     document.getElementById(`#autoplay`)play();
+// })
